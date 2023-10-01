9121f69266586e1f6f4bd7377b9ac663d1d043b5
systemboys <systemboys@hotmail.com>
2023-10-01 17:26:06 -0300

    Atualizar o menu. [ajuste 9]

--- GTi_Support.sh
+++ GTi_Support.sh
@@ -112,11 +112,12 @@ case $option in
         ;;
     dm) # Deletar o menu
         clear
+        cd ..
         echo -e "${vermelho}╭─────────────────────╮${reset}"
         echo -e "${vermelho}│ ${piscando}${negrito}Deletando o Menu...${reset} ${vermelho}│${reset}"
         echo -e "${vermelho}╰─────────────────────╯${reset}"
         sleep ${sleep}
-        cd .. && rm -rf Installation_menu && clear
+        rm -rf Installation_menu && clear
         echo -e "${verde}╭────────────────────────────╮${reset}"
         echo -e "${verde}│ ${piscando}${negrito}Menu deletado com sucesso!${reset} ${vermelho}│${reset}"
         echo -e "${verde}╰────────────────────────────╯${reset}"
